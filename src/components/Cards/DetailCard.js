7eec2eb4b0a47623136a11729bc517489675953f
Eduardo Salinas <eduardo.salinas1@outlook.com>
2021-08-25 05:25:58 -0300

    update

--- src/components/Cards/DetailCard.js
+++ src/components/Cards/DetailCard.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Spinner } from 'react-bootstrap';
-import { getHeroById } from '../../redux/actions';
+import { Card, Spinner, Button } from 'react-bootstrap';
+import { addHeroTeam, getHeroById } from '../../redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
+import { StyledDetail } from './styled';
+import good from '../../assets/good.jpg';
+import bad from '../../assets/bad.jpg';
+import swal from 'sweetalert';
 
 const HeroDetail = ({ match }) => {
     const dispatch = useDispatch();
@@ -12,6 +16,38 @@ const HeroDetail = ({ match }) => {
         dispatch(getHeroById(match.params.id))
     }, [dispatch, match.params.id]);
 
+    const handleClick = () => {
+        if (store.team.length >= 6) {
+            swal({
+                title: "I'm sorry",
+                text: "You already have six heroes on your team!",
+                icon: "warning",
+                dangerMode: true,
+            });
+        } else if (store.heroDetail.biography.alignment === "good" && store.alignment.good >= 3) {
+            swal({
+                title: "I'm sorry",
+                text: "You already have three heroes good your team!",
+                icon: "warning",
+                dangerMode: true,
+            });
+        } else if (store.heroDetail.biography.alignment === "bad" && store.alignment.bad >= 3) {
+            swal({
+                title: "I'm sorry",
+                text: "You already have three heroes bad on your team!",
+                icon: "warning",
+                dangerMode: true,
+            });
+        } else {
+            swal({
+                text: "Hero added to team",
+                icon: "success",
+            });
+            dispatch(addHeroTeam(store.heroDetail))
+        }
+    };
+    const title = store.heroDetail.biography && (store.heroDetail.biography.alignment === "good" ? "title-good" : "title-bad");
+
     return (
         <div>
             {store.loading ?
@@ -19,17 +55,20 @@ const HeroDetail = ({ match }) => {
                     <Spinner animation="border" variant="warning" />
                 </div>
                 :
-                (
-                    <div className="detail">
-                        <img
-                            src={store.heroDetail.image.url}
-                            alt="not found"
-                        />
-                        <Card border="danger" style={{ width: '50rem' }}>
+
+                store.heroDetail.name && (
+                    <StyledDetail>
+                        <Card border="dark" >
+                            <Card.Img className="hero"
+                                src={store.heroDetail.image.url}
+                                alt="not found"
+                            />
+                        </Card>
+                        <Card className="detail" border="dark">
                             <Card.Body>
-                                <Card.Title>{store.heroDetail.name}</Card.Title>
+                                <Card.Text className={title}>{store.heroDetail.name}</Card.Text>
                                 <br />
-                                <Card.Text>⭐<b>Power Stats:</b></Card.Text>
+                                <Card.Text className="power">⭐<b>Power Stats:</b></Card.Text>
                                 <Card.Text>
                                     🧠 Intelligence: {store.heroDetail.powerstats.intelligence} <br />
                                     💪 Strength : {store.heroDetail.powerstats.strength} <br />
@@ -38,11 +77,21 @@ const HeroDetail = ({ match }) => {
                                     💥 Power: {store.heroDetail.powerstats.power} <br />
                                     🥊 Combat: {store.heroDetail.powerstats.combat} <br />
                                 </Card.Text>
+                                <Card.Text>🧔 Occupation: {store.heroDetail.work.occupation}</Card.Text>
                                 <br />
-                                <Card.Text>⚗ Occupation: {store.heroDetail.work.occupation}</Card.Text>
+                                <div className="end">
+                                    <div>
+                                        {store.heroDetail.biography.alignment === "good" ?
+                                            <Card.Text className="good-detail">Good<img src={good} alt="" /></Card.Text> :
+                                            <Card.Text className="bad-detail">Bad<br /><img src={bad} alt="" /></Card.Text>}
+                                    </div>
+                                    <div className="add-detail">
+                                        <Button variant="warning" onClick={handleClick} style={{ width: '5rem' }}>Add Hero to team ➕</Button>
+                                    </div>
+                                </div>
                             </Card.Body>
                         </Card>
-                    </div>
+                    </StyledDetail>
                 )}
         </div>
     )
--- src/components/Cards/styled.js
+++ src/components/Cards/styled.js
@@ -61,6 +61,74 @@ export const StyledAdd = styled.div`
         display:flex;
         justify-content: center;
         align-items: center;        
+    } 
+`
+export const StyledDetail = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin:.5rem;
+    @media screen and (max-width: 1024px){
+        flex-direction: column;
     }
- 
+    .hero{
+        height: 45rem;
+        width: 35rem;
+    }
+    .detail{
+        text-align:center;
+        background-color: rgb(247, 247, 247,0.6);  
+        height: 45rem;
+        width: 35rem;
+        font-size:1.2rem;
+        .title-bad{
+            background-color: #DF2E2E;
+            border:solid black;
+            width: 100%;
+            font-size:2.5rem;
+        }
+        .title-good{
+            background-color: #64C9CF;
+            border:solid black;
+            width: 100%;
+            font-size:2.5rem;
+        }
+        .end{
+            display:flex;
+    justify-content: space-around;
+    align-items: center;
+        }
+.bad-detail{
+    display:flex;
+        justify-content: center;
+        align-items: center;           
+        flex-direction: column;
+        width: 6rem;
+        height: 6rem;
+        border-radius:50%;
+        border:solid black;
+        background-color: #DF2E2E;  
+        img{
+            width:2rem;
+        }
+}
+.good-detail{
+    display:flex;
+        justify-content: center;
+        align-items: center;   
+        flex-direction: column;
+        width: 6rem;
+        height: 6rem;
+        border-radius:50%;
+        border:solid black;
+        background-color: #64C9CF;
+        img{
+            width:2rem;
+        }
+}
+.add-detail{
+    border-radius:.2rem;
+    border:solid black;
+}
+}
 `
